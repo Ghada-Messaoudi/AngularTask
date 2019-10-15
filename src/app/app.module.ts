236066eafd50f6a463e491cc93c0c77883379f3b
Ghada Messaoudi <messaoudighada5@gmail.com>
2019-10-15 23:33:56 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './main/weather.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherTypeComponent } from './weather-infos/weather-type.component';
+import {FormsModule} from '@angular/forms';
+
+const appRoutes: Routes = [
+  {
+    path: 'weather',
+    component: WeatherComponent
+  },
+  { path: '',
+    redirectTo: '/weather',
+    pathMatch: 'full'
+  },
+  { path: '**', component: WeatherComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    WeatherTypeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
